abcd1740637acb07e43b3df4622868268f5a90fd
hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
2022-03-20 13:26:56 -0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+# curl "https://raw.githubusercontent.com/hello-smile6/init-git/main/main.sh"|bash
+# Configure git
+curl "https://raw.githubusercontent.com/hello-smile6/init-git/main/config-git-info.sh"|bash
